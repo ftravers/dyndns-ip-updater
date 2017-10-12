5e6d9d5805136d8cfb5276b4adeb6540e9138267
Fenton Travers <fenton.travers@gmail.com>
2017-10-12 16:41:20 -0700

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dyndns-ip-updater
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dyndns_ip_updater/core_test.clj
@@ -0,0 +1,7 @@
+(ns dyndns-ip-updater.core-test
+  (:require [clojure.test :refer :all]
+            [dyndns-ip-updater.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
